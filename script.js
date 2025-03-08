163441ef7555108631920325d83e152c45202155
V-Yashasvi <vyashasvi1@gmail.com>
2025-03-08 10:18:33 +0530

    Completed the task

--- script.js
+++ script.js
@@ -20,4 +20,15 @@ document.addEventListener('DOMContentLoaded', function() {
   // 4. Display the count on the webpage
 
   // your code here
+
+  let count = getCookie("count");
+  if(count){
+    count=parseInt(count)+1;
+  }
+  else{
+    count=1
+  }
+  setCookie("count", count, 30)
+
+  document.getElementById("countDisplay").textContent=count
 });
